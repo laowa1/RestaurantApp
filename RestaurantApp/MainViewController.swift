128256a1e074dd3092972c342f56f1f86912ca10
Laowai <k.zhalgas@icloud.com>
2021-04-02 23:18:44 +0600

    Add NewPlaceViewController with saveButton -> segue to -> MainViewController

--- RestaurantApp/MainViewController.swift
+++ RestaurantApp/MainViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MainViewController: UITableViewController {
     
-    let place = PlaceModel.getPlaces()
+    var place = PlaceModel.getPlaces()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,56 +30,22 @@ class MainViewController: UITableViewController {
         cell.nameOfPlace.text = places.name
         cell.locationOfPlace.text = places.location
         cell.typeOfPlace.text = places.type
+        if places.image == nil {
+            cell.imageOfPlace.image = UIImage(named: places.restaurantImage!)
+        } else {
+            cell.imageOfPlace.image = places.image
+        }
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.height / 2
-        cell.imageOfPlace.image = UIImage(named: places.image)
+        
         
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+   
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        guard let newPlaceVC = segue.source as? NewPlaceViewController else { return }
+        newPlaceVC.saveNewPlace()
+        place.append(newPlaceVC.newPlace!)
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    @IBAction func cancelAction(_ segue: UIStoryboardSegue) {}
 }
--- RestaurantApp/PlaceModel.swift
+++ RestaurantApp/PlaceModel.swift
@@ -5,13 +5,14 @@
 //  Created by Bend3r on 27.03.2021.
 //
 
-import Foundation
+import UIKit
 
 struct PlaceModel {
     let name: String
-    let type: String
-    let location: String
-    let image: String
+    let type: String?
+    let location: String?
+    let image: UIImage?
+    let restaurantImage: String?
     
    static let restaurantNames = [
         "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
@@ -23,7 +24,7 @@ struct PlaceModel {
    static func getPlaces() -> [PlaceModel] {
         var places = [PlaceModel]()
         for place in restaurantNames {
-            places.append(PlaceModel(name: place, type: "Restaurant", location: "Almaty", image: place))
+            places.append(PlaceModel(name: place, type: "Restaurant", location: "Almaty", image: nil, restaurantImage: place))
         }
         return places
     }
